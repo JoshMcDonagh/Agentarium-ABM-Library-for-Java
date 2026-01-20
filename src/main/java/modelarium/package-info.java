5937fb33e1a438529cf1909dd2566905c4f3e556
JoshMcDonagh <joshua.mcdonagh.99@gmail.com>
2026-01-20 15:53:47 +0000

    Begin adding package-level documentation

---
+++ src/main/java/modelarium/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>Modelarium API.</p>
+ *
+ * <p>This package contains core abstractions for defining models, clocks,
+ * model elements, and general simulation configuration.</p>
+ *
+ */
+package modelarium;
